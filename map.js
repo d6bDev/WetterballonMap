e2c658460e49fab398edb5bf19dbd3d9a2eb19c1
d6bDev <65654153+d6bDev@users.noreply.github.com>
2023-03-14 20:00:38 +0100

    Update map.js

--- map.js
+++ map.js
@@ -1,16 +1,14 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 14);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors', maxZoom: 18,
 }).addTo(map);
 
 map.on('click', onClick);
 
 var popup = L.popup().setContent('<button>Confirm Position</button>');
 
-var test = L.marker([51.505, -0.09]).addTo(map)
-    .bindPopup(popup)
-    .openPopup();
+var test = L.marker([51.505, -0.09]).addTo(map).bindPopup(popup);
 
 function onClick(e) {
     test.setLatLng(e.latlng);
