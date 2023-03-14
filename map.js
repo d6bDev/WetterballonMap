d83385da37ddf003e6b3ac30079b65246ec4eef4
d6bDev <65654153+d6bDev@users.noreply.github.com>
2023-03-14 18:26:46 +0100

    Add files via upload

---
+++ map.js
@@ -0,0 +1,15 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+map.on('click', onClick);
+
+var test = L.marker([51.505, -0.09]).addTo(map)
+    .bindPopup('Test')
+    .openPopup();
+
+function onClick(e) {
+    test.setLatLng(e.latlng);
+}
