bd3b8aff7cf7bf8776f5300e4fd3978bf23592f8
jhuesers <75500144+jhuesers@users.noreply.github.com>
2023-03-15 11:44:41 +0100

    Update database.php added query

--- database.php
+++ database.php
@@ -12,6 +12,15 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "<script> console.log('Connected to DB successfully'); </script>";
+// send Query
+$sql = "INSERT INTO ". $db_dbname ." (firstname, lastname, email, lat, lng)
+VALUES ('John', 'Doe', 'john@example.com',". $_COOKIE["lat"] .", ". $_COOKIE["lng"] .")";
 
-$_COOKIE["lat"]
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
