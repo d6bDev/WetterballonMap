099a610c913924b61479f7e5fd87cfe23c8c65b2
jhuesers <75500144+jhuesers@users.noreply.github.com>
2023-03-16 20:24:13 +0100

    ap.js updatet

--- map.js
+++ map.js
@@ -1,10 +1,10 @@
-
+var map = L.map('map').setView([51.505, -0.09], 14);
 var popup = L.popup().setContent('<button>Confirm Position</button>');
-
 var test = L.marker([51.505, -0.09]).addTo(map).bindPopup(popup);
 
+
 function loadmap(){
-    var map = L.map('map').setView([51.505, -0.09], 14);
+
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors', maxZoom: 18,
