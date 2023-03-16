e6240f5af5115f3c40b42638e38f23e5344e5828
d6bDev <65654153+d6bDev@users.noreply.github.com>
2023-03-16 21:02:03 +0100

    Update map.js

--- map.js
+++ map.js
@@ -1,54 +1,46 @@
-var map = L.map('map').setView([51.505, -0.09], 14);
-var popup = L.popup().setContent('<button>Confirm Position</button>');
-var test = L.marker([51.505, -0.09]).addTo(map).bindPopup(popup);
+function loadmap() {
+    let tempcoords = {lat: 52.70253708487367, lng: 7.294074558464667}
 
-
-function loadmap(){}
-
+    var map = L.map('map').setView([52.70253708487367, 7.294074558464667], 14);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors', maxZoom: 18,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors', maxZoom: 18,
     }).addTo(map);
 
     map.on('click', onClick);
 
+    var popup = L.popup().setContent('<button>Confirm Position</button>');
 
-function onClick(e) {
-    test.setLatLng(e.latlng);
+    var test = L.marker([52.70253708487367, 7.294074558464667]).addTo(map).bindPopup(popup);
 
-    //put latlng in cookies
-    console.log(e.latlng);
-    const latlng = e.latlng;
-    console.log(latlng);
-    /*
-    window.sessionStorage.setItem("lat",e.lating);
-    window.sessionStorage.setItem("lng",e.lating);
-    */
+
+    function onClick(e) {
+        //move marker position to selected position
+        test.setLatLng(e.latlng);
+
+        //save selected lat & lng to temp storage
+        console.log("latitude: " + e.latlng.lat + ", longitude: " + e.latlng.lng);
+
+        tempcoords.lat = e.latlng.lat
+        tempcoords.lng = e.latlng.lng
+    }
+
+    //createcookies
+    function setCookies(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function makeCookies() {
+        //Geodata
+        setCookies("lat", tempcoords.lat, 1);
+        setCookies("lng", tempcoords.lng, 1);
+    }
 }
 
-//createcokies
-function setCookies(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-  
-function makeCookies(){
-    //getVal();
-    
-    var latlng;
-    //Geodata
-    setCookies("lat", latlng,1);// Muss noch heraus finden welcher data typ e.latlng hat 
-    setCookies("lng", latlng,1);
-
-
-}
-
-
-
-function init(){
-    loadmap();
-
+function init() {
+    loadmap()
     console.log("Website geladen");
 }
